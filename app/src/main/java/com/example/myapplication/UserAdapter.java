cc1511c66874949324b29d1e1600d6f1c5234195
FanAnhLiem <quan44188@gmail.com>
2024-10-07 19:23:03 +0700

    UserAdapter.java

--- app/src/main/java/com/example/myapplication/UserAdapter.java
+++ app/src/main/java/com/example/myapplication/UserAdapter.java
@@ -15,7 +15,6 @@ public class UserAdapter extends BaseAdapter {
     private Context context;
     private List<User> userList;
 
-    // Constructor
     public UserAdapter(Context context, List<User> userList) {
         this.context = context;
         this.userList = userList;
@@ -38,7 +37,6 @@ public class UserAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // Sử dụng LayoutInflater để ánh xạ view từ file XML
         if (convertView == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.user_item, parent, false);
         }
@@ -46,20 +44,17 @@ public class UserAdapter extends BaseAdapter {
         // Lấy thông tin người dùng hiện tại
         User user = (User) getItem(position);
 
-        // Ánh xạ các TextView trong user_item.xml
         TextView emailTextView = convertView.findViewById(R.id.detail_email);
         TextView usernameTextView = convertView.findViewById(R.id.detail_username);
         TextView roleTextView = convertView.findViewById(R.id.detail_role);
         TextView genderTextView = convertView.findViewById(R.id.detail_gender);
         Button editButton = convertView.findViewById(R.id.edit_button);
 
-        // Set dữ liệu cho các TextView
         emailTextView.setText(user.getEmail());
         usernameTextView.setText(user.getUsername());
         roleTextView.setText(user.getRole());
         genderTextView.setText(user.getGender());
 
-        // Xử lý sự kiện khi nhấn nút "Chỉnh sửa"
         editButton.setOnClickListener(v -> {
             Intent intent = new Intent(context, UserDetail.class);
             intent.putExtra("email", user.getEmail());
